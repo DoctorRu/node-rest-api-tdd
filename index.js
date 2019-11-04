a225896ad93c714d83611e3021474ff0f9d8d15c
DoctorRu <vjblan@gmail.com>
2019-11-03 22:16:33 -0300

    GET requests / query params

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+    { id: 1, name: 'course 1'},
+    { id: 2, name: 'course 2'},
+    { id: 3, name: 'course 3'}
+]
 app.get('/', (req, res) => {
     res.send('Pong!!!');
 })
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3]);
+    res.send(courses);
+})
+
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('Course not found!')   
+    res.send(course);
+})
+
+app.get('/api/posts/:year/:month', (req, res) => {
+    res.send(req.params);
+})
+
+app.get('/api/posts', (req, res) => {
+    res.send(req.query);
 })
 
 const port = process.env.EXPRESS_PORT || 3000
